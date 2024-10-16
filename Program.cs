874618cba3174c20242b3d0d1b589004e2a9f0c8
LukasTraut <lukastraut@hotmail.com>
2024-10-16 14:43:55 +0200

    .

--- Program.cs
+++ Program.cs
@@ -12,33 +12,29 @@ namespace anmeldung_Lukas_Levin
         const string dataPath = @"C:\Users\levin\OneDrive - sluz\Basislehrjahr\Datenmodelieren\Anmeldungen\Anmeldung_readeble.csv";
         static void Main(string[] args)
         {
-            string[] content = File.ReadAllLines(dataPath); 
+            string[] lines = File.ReadAllLines(dataPath); 
             Console.WriteLine("Test");
-            Console.Write(content);
-
-
-
-
-
-
+            Console.Write(lines);
 
             Console.WriteLine("*** Anmeldung verwalten ***");
             Console.WriteLine("Menu: a = Anmeldungen anzeigen, n = Neue Anmeldung, g = Gebrauchte Vorlesungen");
-            string a = Console.ReadLine();
-            string[] lines = content;
-            string[] inf = infos;
-            inf = spl
-            for (int )
-            {
-
+            string menu = Console.ReadLine();
+            
+            
+            
+            if (menu == "a" || menu == "A") {
+                for (int i = 0; i < lines.Length; i++)
+                {
+                    
+                }
             }
-            string n = Console.ReadLine();
-            else if (n == "n" || n == "N")
-            {
+            else if (menu == "n" || menu == "N")
+                {
 
 
 
-            }
+                }
+            
         }
         static string newUser (string content) {
             string n = Console.ReadLine();
