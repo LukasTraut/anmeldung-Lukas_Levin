b05f6bd4f71b56f0681c0f3f76804e20acfe9155
Levin Schmid <levin.schmid@mein.gmx>
2024-10-16 14:01:00 +0200

    Add File.csv

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace anmeldung_Lukas_Levin
 {
@@ -10,7 +11,9 @@ namespace anmeldung_Lukas_Levin
     {
         static void Main(string[] args)
         {
+            string content = File.ReadAllText(@"C:\Users\levin\OneDrive - sluz\Basislehrjahr\Datenmodelieren\Anmeldungen\Anmeldung_readeble.csv");
             Console.WriteLine("Test");
+            Console.Write(content);
         }
     }
 }
